c8e3848be1377c8e9482a48fc501ecb5083cab25
gnourt0307 <truongtkls8@gmail.com>
2026-03-01 01:35:08 +0700

    fix isWeekend function

--- backend/utils/isWeekend.js
+++ backend/utils/isWeekend.js
@@ -1,12 +1,14 @@
-function isWeekend() {
-  const day = new Date();
-  const dayOfWeek = day.getDay();
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
 
-  if (dayOfWeek === 0 || dayOfWeek === 6) {
-    return true;
-  } else {
-    return false;
-  }
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+function isWeekend() {
+  const dayOfWeek = dayjs().tz("Asia/Ho_Chi_Minh").day();
+
+  return dayOfWeek === 0 || dayOfWeek === 6;
 }
 
 module.exports = isWeekend;
